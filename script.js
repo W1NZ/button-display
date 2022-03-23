5900b5a8e46f7f6814028471658c41235e048ef1
Mariusz Czapiewski <92310223+W1NZ@users.noreply.github.com>
2022-03-23 20:56:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const p1 = document.querySelector(".p1")
+const p2 = document.querySelector(".p2")
+const p3 = document.querySelector(".p3")
+const p4 = document.querySelector(".p4")
+const wyswietl = document.querySelector(".wyswietl")
+
+
+p1.addEventListener("click",function(){
+    wyswietl.style.backgroundImage = "url(pictures/rhpAWNI.png)"
+})
+
+p2.addEventListener("click",function(){
+    wyswietl.style.backgroundImage = "url(pictures/7gUFAC8.png)"
+})
+
+p3.addEventListener("click",function(){
+    wyswietl.style.backgroundImage = "url(pictures/P9UesXr.png)"
+})
+
+p4.addEventListener("click",function(){
+    wyswietl.style.backgroundImage = "url(pictures/x8rXOd0.png)"
+})
